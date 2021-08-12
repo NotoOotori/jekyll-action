0cb1156f3bba73c0ce4df04b121f636a0eed83f9
Chen Xuyang <39304463+NotoOotori@users.noreply.github.com>
2021-08-12 14:19:46 +0900

    use github-actions[bot] to commit

--- entrypoint.sh
+++ entrypoint.sh
@@ -144,8 +144,8 @@ touch .nojekyll
 
 echo "Publishing to ${GITHUB_REPOSITORY} on branch ${remote_branch}"
 
-git config user.name "${GITHUB_ACTOR}" && \
-git config user.email "${GITHUB_ACTOR}@users.noreply.github.com" && \
+git config user.name "github-actions" && \
+git config user.email "<41898282+github-actions[bot]@users.noreply.github.com>" && \
 git add . && \
 git commit $COMMIT_OPTIONS -m "jekyll build from Action ${GITHUB_SHA}" && \
 git push $PUSH_OPTIONS $REMOTE_REPO $LOCAL_BRANCH:$remote_branch && \
